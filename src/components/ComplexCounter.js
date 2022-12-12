9d5f31bd69f770eeeeaec4f1e3e62f7534a4435a
Muhammad SA <MuhammadSA211999>
2022-12-12 22:36:13 +0600

    complex example

---
+++ src/components/ComplexCounter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useReducer } from 'react';
+const initialState = 0
+const reducer = (state, action) => {
+    switch (action) {
+        case "increament":
+            return state + 1;
+        case "decreament":
+            return state - 1
+        default:
+            return state
+    }
+}
+const ComplexCounter = () => {
+    const [count, dispatch] = useReducer(reducer, initialState)
+    return (
+        <div>
+            <div>count-{count}</div>
+            <button onClick={() => dispatch("increament")} type='button'>Increament</button>
+            <button onClick={() => dispatch("decreament")} type='button'>Decreament</button>
+        </div>
+    );
+};
+
+export default ComplexCounter;
