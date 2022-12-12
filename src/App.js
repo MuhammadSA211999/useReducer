3579c5bb90ca39896b10431f74a5d3aba106fd38
Muhammad SA <MuhammadSA211999>
2022-12-12 23:04:14 +0600

    complex counter with obj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import ComplexCounter from './components/ComplexCounter';
 
 function App() {
   return (
     <div className="App">
       <h1>Explore useReducer from Learn with Sumit (LWS)</h1>
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ComplexCounter></ComplexCounter>
     </div>
 
   );
--- src/components/ComplexCounter.js
+++ src/components/ComplexCounter.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useReducer } from 'react';
-const initialState = 0
+const initialState = {
+    counter: 0,
+    counter2: 0
+
+}
 const reducer = (state, action) => {
-    switch (action) {
-        case "increament":
-            return state + 1;
-        case "decreament":
-            return state - 1
+    switch (action.type) {
+        case "increament1":
+            return { ...state, counter: state.counter + action.count };
+        case "decreament1":
+            return { ...state, counter: state.counter - action.count }
+        case "increament2":
+            return { ...state, counter2: state.counter2 + action.count };
+        case "decreament2":
+            return { ...state, counter2: state.counter2 - action.count }
         default:
             return state
     }
@@ -15,9 +23,16 @@ const ComplexCounter = () => {
     const [count, dispatch] = useReducer(reducer, initialState)
     return (
         <div>
-            <div>count-{count}</div>
-            <button onClick={() => dispatch("increament")} type='button'>Increament</button>
-            <button onClick={() => dispatch("decreament")} type='button'>Decreament</button>
+            <div>
+                <div>count-{count.counter}</div>
+                <button onClick={() => dispatch({ type: "increament1", count: 1 })} type='button'>Increament</button>
+                <button onClick={() => dispatch({ type: "decreament1", count: 1 })} type='button'>Decreament</button>
+            </div>
+            <div>
+                <div>count2-{count.counter2}</div>
+                <button onClick={() => dispatch({ type: "increament2", count: 2 })} type='button'>Increament</button>
+                <button onClick={() => dispatch({ type: "decreament2", count: 2 })} type='button'>Decreament</button>
+            </div>
         </div>
     );
 };
